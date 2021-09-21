d667afbd986cd26003a92661fef7d7e38080c1e6
Vinny-86 <vinisilva.nh@gmail.com>
2021-09-21 16:14:39 -0300

    finalização do projeto

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,5 +1,68 @@
 import styled from 'styled-components';
+import Slider from 'react-slick';
 
-export const Container = styled.div`
-	//	background-color: ${props => props.theme.colors.primary};
+export const Wrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+`;
+
+export const Container = styled.aside`
+	background-color: ${props => props.theme.colors.background};
+	width: 360px;
+	height: 100vh;
+	overflow-y: auto;
+`;
+
+export const Search = styled.section`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	background-color: white;
+	padding: 16px;
+`;
+
+export const Logo = styled.img`
+	margin-bottom: 15px;
+`;
+
+export const Map = styled.div`
+	background-color: red;
+	width: 500px;
+`;
+
+export const Carousel = styled(Slider)`
+	.slick-slide {
+		margin-right: 30px;
+	}
+`;
+
+export const CarouselTitle = styled.h1`
+	font-family: ${props => props.theme.fonts.regular};
+	color: ${props => props.theme.colors.text};
+	font-size: 24px;
+	font-weight: bold;
+	line-height: 28px;
+	margin: 14px 0;
+`;
+
+export const ModalTitle = styled.p`
+	margin-left: 10px;
+	margin-top: 10px;
+	margin-bottom: 10px;
+	letter-spacing: 0.11px;
+	font-family: ${props => props.theme.fonts.regular};
+	color: ${props => props.theme.colors.text};
+	line-height: 29px;
+	font-size: 24px;
+	font-weight: bold;
+`;
+
+export const ModalContent = styled.p`
+	margin-left: 10px;
+	margin-bottom: 10px;
+	font-family: ${props => props.theme.fonts.regular};
+	color: ${props => props.theme.colors.text}
+	font-size: 16px;
+	line-height: 19px;
+	font-weight: normal;
 `;
